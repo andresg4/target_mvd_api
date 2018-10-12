6d7df83702d9a98e2b65a3723cb7336a6bf5dbc1
Andres Garcia <andres@rootstrap.com>
2018-10-12 12:23:29 -0300

    Remove commented code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::API
-  # protect_from_forgery with: :exception
   before_action :authenticate_user!
   rescue_from ArgumentError, with: :record_not_found
 
