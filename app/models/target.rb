afa284755d437ff912eb18bed6423317fdd8d348
Andres <andresg123@gmail.com>
2018-11-01 18:07:06 -0300

    Add targets limit per user
    * Add targets limit per user

--- app/models/target.rb
+++ app/models/target.rb
@@ -1,4 +1,6 @@
 class Target < ApplicationRecord
+  LIMIT_TARGETS = 10
+
   belongs_to :topic
   belongs_to :user
   acts_as_mappable default_units: :kms,
@@ -10,4 +12,14 @@ class Target < ApplicationRecord
   validates :title, uniqueness: true
 
   validates :radius, numericality: { greater_than_or_equal_to: 0 }
+
+  validate :validate_user_limit, on: :create
+
+  private
+
+  def validate_user_limit
+    return if user && user.targets.count < LIMIT_TARGETS
+
+    errors.add(:user, :exceeded_quota)
+  end
 end
