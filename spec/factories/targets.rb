9bf3178c36b791a7a1d47643af2ff257760261f0
Andres Garcia <andres@rootstrap.com>
2018-11-22 16:47:32 -0300

    Change unit to meters and minimum of target radius

--- spec/factories/targets.rb
+++ spec/factories/targets.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :target, class: Target do
     sequence(:title)  { |n| "#{n} #{Faker::Job.field[0, 15]}" }
-    radius            { Faker::Number.between(1, 100) }
+    radius            { Faker::Number.between(20, 1000) }
     latitude          { Faker::Address.latitude }
     longitude         { Faker::Address.longitude }
     topic
