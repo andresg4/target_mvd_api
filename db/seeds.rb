fed7a16f7b2faa8ec7b3076dead9544fbda82fc7
Andres Garcia <andres@rootstrap.com>
2018-12-04 11:03:38 -0300

    Fixed seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# This file should contain all the record creation needed to seed the database with
+# its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database
+# with db:setup).
 #
 # Examples:
 #
@@ -10,4 +12,4 @@ topics_list = %w[Football Travel Politics Art Dating Music Movies Series Food]
 topics_list.each do |name|
   Topic.create(name: name)
 end
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password') if Rails.env.development?
